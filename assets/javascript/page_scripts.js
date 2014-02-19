7e097443b2d6900a0b2da6228bb1733ed5fe3d8c
oZ0614 <omari.wallace@gmail.com>
2014-02-18 22:34:10 -0500

    Added functionality for multiple days and switching days.  Need fxns to remove items, limit 1 hotel/day, limit 3 restaruants/day, add marker colors/icons

--- assets/javascript/page_scripts.js
+++ assets/javascript/page_scripts.js
@@ -5,7 +5,7 @@ function initialize_gmaps() {
     // set the map options hash
   var mapOptions = {
     center: myLatlng,
-    zoom: 10,
+    zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
@@ -30,14 +30,63 @@ $(document).ready(function() {
 });
 
 /** Day Button Functions **/
+$(document).ready(function() {
+  $('#day_buttons').on('click', 'button',function() {
+    // !!! the $(this) allows you to access the button that was clicked !!!
+    clearItems(window.day_no);
+    window.day_no = $(this).text().slice(-1);
+    $("#daily_itinerary h2").text("Plan for Day "+day_no);
+    showItems(window.day_no);
+  })
+})
+
+/** Add Itinerary Item Functions **/
+var daily_plan = {};
+var DayCreator = function (day_no) {
+  daily_plan[day_no] = {hotels: [], things_to_do: [], restaurants: [], markers: []}
+}
+
+/** Clear Items Functionality **/
+function clearItems(day_no) {
+  $("#hotel_itinerary li").remove()
+  $("#things_to_do_itinerary li").remove()
+  $("#restaurant_itinerary li").remove()
+  for (var i = 0; i < daily_plan[day_no]['markers'].length; i++ ) {
+    daily_plan[day_no]['markers'][i].setMap(null);
+  }
+}
+
+/** Add Items on Day **/
+function showItems (day_no) {
+  // Array storing the (1) key from the daily_plan object and the (2) related DOM element
+  var itinerariries = [['hotels','#hotel_itinerary'], ['things_to_do','#things_to_do_itinerary'], ['restaurants','#restaurant_itinerary']];
+  for (j=0; j<itinerariries.length; j++) {
+  	var item = itinerariries[j]
+  	console.log(item);
+    for (i=0; i<daily_plan[day_no][item[0]].length; i++) {
+      $(item[1]).append('<li>'+daily_plan[day_no][item[0]][i].place[0].name+'</li>');
+    }
+  }
+  for (i=0; i<daily_plan[day_no]['markers'].length; i++) {
+    daily_plan[day_no]['markers'][i].setMap(map);
+  }
+}
+
+
+// Adding a day
 var day_no = 1;
 $(document).ready(function() {
-    $('#add_day').on('click', function() {
-      if (day_no < 3) {
-        $('#day_buttons').append('<button type="button" class="btn btn-default">Day'+(day_no+1).toString()+'</button>');
-        day_no += 1;
-      }
-    });
+  DayCreator(day_no)
+  $('#add_day').on('click', function() {
+    day_no = parseInt(day_no, 10);
+    clearItems(day_no);
+    if (day_no < 3) {
+      $('#day_buttons').append('<button type="button" class="btn btn-default">Day '+(day_no+1)+'</button>');
+      day_no += 1;
+    }
+    DayCreator(day_no)
+    alert(day_no)
+  });
 });
 
 /** Drop-down functions **/
@@ -54,22 +103,19 @@ for (var i=0; i<all_restaurants.length; i++) {
   $('#restaurants_list').append('<option value="restaurant_'+i+'">'+all_restaurants[i].place[0].name+'</option>')
 }
 
-/** Add Itinerary Item Functions **/
-var daily_plan = {
-  hotels: [],
-  things_to_do: [],
-  restaurants: [],
-}
+
 // Add Hotels to Itinerary
 $(document).ready(function() {
   $('#add_hotel').on('click', function() {
     // The ".val()" method below allows you access the selected item in the '#hotels_list' dropdown list (which is the select tag)
+    var daily_plan = window.daily_plan;
+    var day_no = window.day_no.toString();
     var selected_value = $('#hotels_list').val().split("_")[1];
-    daily_plan['hotels'].push(hotel_object)
     var hotel_object = all_hotels[selected_value]
     var hotel_name = hotel_object.place[0].name
-    $('#hotel_itinerary').append('<li>'+hotel_name+'</li>');
+    daily_plan[day_no]['hotels'].push(hotel_object)
     console.log(daily_plan);
+    $('#hotel_itinerary').append('<li>'+hotel_name+'</li>');
     // Add the hotel marker to the map
     var latitude = hotel_object.place[0].location[0]
     var longitude = hotel_object.place[0].location[1]
@@ -80,15 +126,20 @@ $(document).ready(function() {
       animation: google.maps.Animation.DROP
     });
     // Add the marker to the map by calling setMap()
+    daily_plan[day_no]['markers'].push(marker)
     marker.setMap(map);
   });
 // Add Things to do to Itinerary
   $('#add_thing_to_do').on('click', function() {
+    // The ".val()" method below allows you access the selected item in the '#hotels_list' dropdown list (which is the select tag)
+    var daily_plan = window.daily_plan;
+    var day_no = window.day_no.toString();
     var selected_value = $('#things_to_do_list').val().split("_")[1];
     var things_to_do_object = all_things_to_do[selected_value]
     var things_to_do_name = things_to_do_object.place[0].name
-    daily_plan['things_to_do'].push(things_to_do_object)
+    // window.daily_plan[day_no]['things_to_do'].push(things_to_do_object)
     $('#things_to_do_itinerary').append('<li>'+things_to_do_name+'</li>');
+    daily_plan[day_no]['things_to_do'].push(things_to_do_object)
     console.log(daily_plan);
     var latitude = things_to_do_object.place[0].location[0]
     var longitude = things_to_do_object.place[0].location[1]
@@ -99,15 +150,19 @@ $(document).ready(function() {
       animation: google.maps.Animation.DROP
     });
     // Add the marker to the map by calling setMap()
+    daily_plan[day_no]['markers'].push(marker)
     marker.setMap(map);
   });
 // Add Restaurants to Itinerary
   $('#add_restaurant').on('click', function() {
+    // The ".val()" method below allows you access the selected item in the '#hotels_list' dropdown list (which is the select tag)
+    var daily_plan = window.daily_plan;
+    var day_no = window.day_no.toString();
     var selected_value = $('#restaurants_list').val().split("_")[1];
     var restaurants_object = all_restaurants[selected_value]
     var restaurants_name = restaurants_object.place[0].name
     $('#restaurant_itinerary').append('<li>'+restaurants_name+'</li>');
-    daily_plan['restaurants'].push(restaurants_object)
+    daily_plan[day_no]['restaurants'].push(restaurants_object)
     console.log(daily_plan);
     var latitude = restaurants_object.place[0].location[0]
     var longitude = restaurants_object.place[0].location[1]
@@ -118,6 +173,7 @@ $(document).ready(function() {
       animation: google.maps.Animation.DROP
     });
     // Add the marker to the map by calling setMap()
+    daily_plan[day_no]['markers'].push(marker)
     marker.setMap(map);
   })
 });
